88bd2edccffbb75c6ad117deb3b162f6df0e41b7
zanadoman <domanseven@gmail.com>
2024-07-24 13:00:43 +0200

    shop hitboxes

--- include/game/shop.hpp
+++ include/game/shop.hpp
@@ -1,14 +1,23 @@
 #ifndef GAME_SHOP_HPP
 #define GAME_SHOP_HPP
 
-#include <wizard_engine/wizard_engine.hpp>
 #include <game/player.hpp>
+#include <wizard_engine/wizard_engine.hpp>
 
 class shop final {
   private:
     wze::sprite _background;
 
-    player _player; 
+    wze::collider _top;
+    wze::collider _bottom;
+    wze::collider _left;
+    wze::collider _right;
+    wze::collider _top_left;
+    wze::collider _top_right;
+    wze::collider _bottom_left;
+    wze::collider _bottom_right;
+
+    player _player;
 
   public:
     shop();
--- src/player.cpp
+++ src/player.cpp
@@ -1,34 +1,42 @@
 #include <game/assets.hpp>
 #include <game/player.hpp>
 
-player::player() : collider({{{-16, 0}, {0, 16}, {16, 0}, {0, -16}}}) {
-    _appearance = std::shared_ptr<wze::sprite>(
-        new wze::sprite(x(), y(), wze::camera::focus(), 0, 160, 160, true,
-                        assets::placeholder_texture()));
+player::player() : collider({{{-40, 0}, {0, -40}, {40, 0}, {0, 40}}}, 0, 0, 0) {
+    _appearance = std::shared_ptr<wze::sprite>(new wze::sprite(
+        x(), y(), wze::camera::focus(), 0, 160, 160, true,
+        assets::placeholder_texture(), std::numeric_limits<uint8_t>::max(),
+        std::numeric_limits<uint8_t>::max(),
+        std::numeric_limits<uint8_t>::max(),
+        std::numeric_limits<uint8_t>::max(), wze::FLIP_NONE, true,
+        std::numeric_limits<uint8_t>::max(), true, 0, -40, 0, true, true, false,
+        false, false));
 
     components().push_back(_appearance);
+    recompose();
 }
 
 void player::update() {
     if ((wze::input::key(wze::KEY_W) || wze::input::key(wze::KEY_UP)) &&
         !(wze::input::key(wze::KEY_S) || wze::input::key(wze::KEY_DOWN))) {
-        set_y(y() - wze::timer::delta_time());
+        set_y(y() - 0.75f * wze::timer::delta_time());
     } else if ((wze::input::key(wze::KEY_S) ||
                 wze::input::key(wze::KEY_DOWN)) &&
                !(wze::input::key(wze::KEY_W) || wze::input::key(wze::KEY_UP))) {
-        set_y(y() + wze::timer::delta_time());
+        set_y(y() + 0.75f * wze::timer::delta_time());
     }
 
     if ((wze::input::key(wze::KEY_A) || wze::input::key(wze::KEY_LEFT)) &&
         !(wze::input::key(wze::KEY_D) || wze::input::key(wze::KEY_RIGHT))) {
-        set_x(x() - wze::timer::delta_time());
+        set_x(x() - 0.75f * wze::timer::delta_time());
     } else if ((wze::input::key(wze::KEY_D) ||
                 wze::input::key(wze::KEY_RIGHT)) &&
                !(wze::input::key(wze::KEY_A) ||
                  wze::input::key(wze::KEY_LEFT))) {
-        set_x(x() + wze::timer::delta_time());
+        set_x(x() + 0.75f * wze::timer::delta_time());
     }
 
     wze::camera::set_x(x());
     wze::camera::set_y(y());
+
+    printf("%f, %f\n", (double)x(), (double)y());
 }
